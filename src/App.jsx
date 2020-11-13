454ab03b352765157378e1c6b1c9e2e7f5b276ee
j-wenning <wenning.justin@gmail.com>
2020-11-12 16:48:47 -0800

    Updated app component to include routing as well as header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer';
+import RouteSwitch from './components/RouteSwitch'
 import './App.scss';
 
 export default class App extends React.Component {
-  render () {
+  render() {
     return (
-      <div>
-        <header>
-
-        </header>
-        <main>
-
-        </main>
-      </div>
+      <Router>
+        <Header />
+        <RouteSwitch />
+        <Footer />
+      </Router>
     );
   }
 }
