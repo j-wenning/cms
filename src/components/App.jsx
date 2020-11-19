f1385c0695a9ba5c746eaad1d07645341fedda8d
j-wenning <wenning.justin@gmail.com>
2020-11-18 18:49:52 -0800

    Updated app component location

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer';
-import RouteSwitch from './components/RouteSwitch'
-import './App.scss';
+import Header from './Header'
+import Footer from './Footer';
+import RouteSwitch from './RouteSwitch'
+import '../scss/App.scss';
 
 export default class App extends React.Component {
   render() {
