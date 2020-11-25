4b1f0433c65c99e041a854e83768fcadbb76779f
j-wenning <wenning.justin@gmail.com>
2020-11-24 21:42:05 -0800

    Updated range slider component to recieve and send updates to related value objects Updated component to reduce its bottom margin by the height of its outputs

--- src/components/RangeSlider.jsx
+++ src/components/RangeSlider.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import $ from 'jquery'
 
 export default class RangeSlider extends React.Component {
   constructor(props) {
@@ -6,10 +7,13 @@ export default class RangeSlider extends React.Component {
     this.state = {
       vals: props.vals || [0, 100],
       boundsMin: props.min || 0,
-      boundsMax: props.max || 100
+      boundsMax: props.max || 100,
+      outputHeight: 0
     }
     this.className = props.className || ''
     this.updateVals = props.updateVals || (() => {})
+    this.outRef = React.createRef()
+    this._refs = props.vals.map(() => React.createRef())
   }
 
   updateBounds(e, index) {
@@ -17,6 +21,15 @@ export default class RangeSlider extends React.Component {
     newVals[index] = Number(e.currentTarget.value)
     this.setState({ vals: newVals })
     this.updateVals(newVals)
+  }
+
+  componentDidUpdate() {
+    const outputHeight = $(this.outRef.current).css('height')
+    this._refs.forEach((ref, i) => {
+      if (ref.current) ref.current.value = this.state.vals[i]
+    })
+    if (this.state.outputHeight !== outputHeight)
+    this.setState({ outputHeight })
   }
 
   render() {
@@ -33,26 +46,28 @@ export default class RangeSlider extends React.Component {
     }
     this.state.vals.forEach((val, i) => (rangeStyle['--v' + i] = val))
     return (
-      <div className={'multi-range-container ' + this.className}>
+      <div style={{ '--oh': this.state.outputHeight }} className={'multi-range-container ' + this.className}>
         <div
           role='group'
           aria-labelledby='multi-lbl'
           className='multi-range'
           style={rangeStyle}>
           {
-            this.state.vals.map((val, i) => (
-              <React.Fragment key={i}>
+            this.state.vals.map((val, i) => {
+              return (<React.Fragment key={i}>
                 <label htmlFor={'v' + i} className="sr-only">Value {i}</label>
                 <input
                   onChange={e => this.updateBounds(e, i)}
-                  type='range'
-                  id={'v' + i}
                   min={this.state.boundsMin}
                   defaultValue={val}
-                  max={this.state.boundsMax} />
-                <output htmlFor={'v' + i} style={{ '--c': `var(--v${i})` }} />
+                  max={this.state.boundsMax}
+                  ref={this._refs[i]}
+                  type='range'
+                  id={'v' + i} />
+                <output ref={this.outRef} htmlFor={'v' + i} style={{ '--c': `var(--v${i})` }} />
               </React.Fragment>
-            ))
+              )
+            })
           }
         </div>
       </div>
