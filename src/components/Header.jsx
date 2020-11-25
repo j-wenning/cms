b880e75f0a40eb8614e9beeb97fecb10da40b622
j-wenning <wenning.justin@gmail.com>
2020-11-25 15:46:49 -0800

    Updated header component to use bootstrap icons Updated component to use single quotes and semi colons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,35 @@
-import React from 'react'
-import { SearchIcon } from '@primer/octicons-react'
-import $ from 'jquery'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import $ from 'jquery';
+import { Link } from 'react-router-dom';
 
 export default class App extends React.Component {
-  constructor() {
-    super()
-    this.nav = React.createRef()
-    this.state = { navHeight: 0 }
+  constructor(props) {
+    super(props);
+    this.nav = React.createRef();
+    this.state = { navHeight: 0 };
   }
 
-  componentDidMount() {
-    this.setState({ navHeight: $(this.nav.current).css('height') })
-  }
+  componentDidMount() { this.setState({ navHeight: $(this.nav.current).css('height') }); }
 
   render() {
     return (
       <header style={{paddingTop: this.state.navHeight}}>
-        <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top" ref={this.nav}>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSearch" aria-controls="navbarSearch" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+        <nav className='navbar navbar-expand-md navbar-light bg-light fixed-top' ref={this.nav}>
+          <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSearch' aria-controls='navbarSearch' aria-expanded='false' aria-label='Toggle navigation'>
+            <span className='navbar-toggler-icon'></span>
           </button>
-          <Link to="/" className="navbar navbar-brand mr-0 mr-md-2">Brand</Link>
-          <Link to="/cart" className="navbar navbar-brand order-md-last mr-0">Cart</Link>
-          <div className="collapse navbar-collapse" id="navbarSearch">
-            <ul className="navbar-nav mt-2 mt-lg-0">
+          <Link to='/' className='navbar navbar-brand mr-0 mr-md-2'>Brand</Link>
+          <Link to='/cart' className='navbar navbar-brand order-md-last mr-0' title='Cart'>
+            <img src='/bootstrap/cart.svg' alt='Cart' />
+          </Link>
+          <div className='collapse navbar-collapse' id='navbarSearch'>
+            <ul className='navbar-nav mt-2 mt-lg-0'>
               <li>
-                <form className="input-group">
-                  <input type="text" className="form-control" placeholder="Search" aria-label="Search" />
-                  <div className="input-group-append">
-                    <button className="btn btn-outline-secondary" type="submit">
-                      <SearchIcon />
+                <form className='input-group'>
+                  <input type='text' className='form-control' placeholder='Search' aria-label='Search' />
+                  <div className='input-group-append'>
+                    <button className='btn btn-outline-secondary' type='submit'>
+                      <img src='/bootstrap/search.svg' alt='Search' />
                     </button>
                   </div>
                 </form>
@@ -39,6 +38,6 @@ export default class App extends React.Component {
           </div>
         </nav>
       </header>
-    )
+    );
   }
-}
+};
