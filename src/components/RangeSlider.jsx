1e01f734ec4ad62b86b0e07eb5d38b38fd1152f8
j-wenning <wenning.justin@gmail.com>
2020-11-27 13:25:54 -0800

    Updated range slider component to properly rely upon value array prop Updated component to clean up some extra loops and unnecessary css variables

--- src/components/RangeSlider.jsx
+++ src/components/RangeSlider.jsx
@@ -4,68 +4,52 @@ import $ from 'jquery';
 export default class RangeSlider extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      vals: props.vals || [0, 100],
-      boundsMin: props.min || 0,
-      boundsMax: props.max || 100,
-      outputHeight: 0
-    };
-    this.className = props.className || '';
-    this.updateVals = props.updateVals || (() => {});
+    this.state = { outputHeight: 0 };
     this.outRef = React.createRef();
-    this._refs = props.vals.map(() => React.createRef());
   }
 
   updateBounds(e, index) {
-    const newVals = [...this.state.vals];
-    newVals[index] = Number(e.currentTarget.value);
-    this.setState({ vals: newVals });
-    this.updateVals(newVals);
+    this.props.vals[index] = e.currentTarget.value;
+    this.props.updateVals?.(this.props.vals);
   }
 
-  componentDidUpdate() {
-    const outputHeight = $(this.outRef.current).css('height');
-    this._refs.forEach((ref, i) => {
-      if (ref.current) ref.current.value = this.state.vals[i];
-    });
-    if (this.state.outputHeight !== outputHeight) this.setState({ outputHeight });
-  }
+  componentDidMount() { this.setState({ outputHeight: $(this.outRef.current).css('height') }) }
 
   render() {
-    const rangeStyle = {
-      '--min': this.state.boundsMin,
-      '--max': this.state.boundsMax,
-      '--fill': this.state.vals.map((val, i) => `
-        linear-gradient(
-          90deg,
-          red calc(var(--r) + (var(--v${i}) - var(--min)) / var(--dif) * var(--uw)),
-          transparent 0
-        )
-      `).join(',').replace(/\s+/, ' ')
-    };
-    this.state.vals.forEach((val, i) => (rangeStyle['--v' + i] = val));
     return (
-      <div style={{ '--oh': this.state.outputHeight }} className={'multi-range-container ' + this.className}>
+      <div
+        style={{ '--oh': this.state.outputHeight }}
+        className={'multi-range-container ' + this.props.className}>
         <div
           role='group'
           aria-labelledby='multi-lbl'
           className='multi-range'
-          style={rangeStyle}>
-          {
-            this.state.vals.map((val, i) => {
-              return (<React.Fragment key={i}>
-                <label htmlFor={'v' + i} className="sr-only">Value {i}</label>
-                <input
-                  onChange={e => this.updateBounds(e, i)}
-                  min={this.state.boundsMin}
-                  defaultValue={val}
-                  max={this.state.boundsMax}
-                  ref={this._refs[i]}
-                  type='range'
-                  id={'v' + i} />
-                <output ref={this.outRef} htmlFor={'v' + i} style={{ '--c': `var(--v${i})` }} />
-              </React.Fragment>
+          style={{
+            '--min': this.props.min,
+            '--max': this.props.max,
+            '--fill': this.props.vals.map(val => `
+              linear-gradient(
+                90deg,
+                red calc(var(--r) + (${val} - var(--min)) / var(--dif) * var(--uw)),
+                transparent 0
               )
+            `).join(',').replace(/\s+/, ' ')
+          }}>
+          {
+            this.props.vals.map((val, i) => {
+              return (
+                <React.Fragment key={i}>
+                  <label htmlFor={'v' + i} className="sr-only">Value {i}</label>
+                  <input
+                    onChange={e => this.updateBounds(e, i)}
+                    min={this.props.min}
+                    value={val}
+                    max={this.props.max}
+                    type='range'
+                    id={'v' + i} />
+                  <output ref={this.outRef} htmlFor={'v' + i} style={{ '--c': val }} />
+                </React.Fragment>
+              );
             })
           }
         </div>
