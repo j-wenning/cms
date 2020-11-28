3c6fa4d8f138b7910b7663738b9f30905af29720
j-wenning <wenning.justin@gmail.com>
2020-11-27 16:42:57 -0800

    Updated header component to implement functionality to search input

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import $ from 'jquery';
 import { Link } from 'react-router-dom';
+import { buildQuery } from './URI';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.nav = React.createRef();
-    this.state = { navHeight: 0 };
+    this.state = {
+      navHeight: 0,
+      searchQuery: ''
+    };
   }
 
-  componentDidMount() { this.setState({ navHeight: $(this.nav.current).css('height') }); }
+  handleInput(e) { this.setState({ searchQuery: e.target.value }); }
+
+  resetInput() { this.setState({ searchQuery: '' }); }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const query = buildQuery({ s: this.state.searchQuery }, window.location.search);
+    window.location.replace(`/search` + query);
+  }
+
+  componentDidMount() {
+    this.setState({
+      navHeight: $(this.nav.current).css('height'),
+      searchQuery: new URLSearchParams(window.location.search).get('s')
+    });
+  }
 
   render() {
     return (
@@ -18,15 +37,21 @@ export default class App extends React.Component {
           <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSearch' aria-controls='navbarSearch' aria-expanded='false' aria-label='Toggle navigation'>
             <span className='navbar-toggler-icon'></span>
           </button>
-          <Link to='/' className='navbar navbar-brand mr-0 mr-md-2'>Brand</Link>
-          <Link to='/cart' className='navbar navbar-brand order-md-last mr-0' title='Cart'>
+          <Link onClick={() => this.resetInput()} to='/' className='navbar navbar-brand mr-0 mr-md-2'>Brand</Link>
+          <Link onClick={() => this.resetInput()} to='/cart' className='navbar navbar-brand order-md-last mr-0' title='Cart'>
             <img src='/bootstrap/cart.svg' alt='Cart' />
           </Link>
           <div className='collapse navbar-collapse' id='navbarSearch'>
             <ul className='navbar-nav mt-2 mt-lg-0'>
               <li>
-                <form className='input-group'>
-                  <input type='text' className='form-control' placeholder='Search' aria-label='Search' />
+                <form onSubmit={e => this.handleSubmit(e)} className='input-group'>
+                  <input
+                    onChange={e => this.handleInput(e)}
+                    value={this.state.searchQuery}
+                    className='form-control'
+                    type='text'
+                    placeholder='Search'
+                    aria-label='Search' />
                   <div className='input-group-append'>
                     <button className='btn btn-outline-secondary' type='submit'>
                       <img src='/bootstrap/search.svg' alt='Search' />
