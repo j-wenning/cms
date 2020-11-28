cdd49f2faa0f3aa037fdab0ecd11d2a611cbda99
j-wenning <wenning.justin@gmail.com>
2020-11-27 17:26:29 -0800

    Updated 404 component to use single quotes

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -5,8 +5,8 @@ export default class _404 extends React.Component {
   render() {
     return (
       <div className='container'>
-        <div className="row">
-          <div className="col">
+        <div className='row'>
+          <div className='col'>
             <h1 className='mt-3'>Page Not Found</h1>
             <p className='mt-3'>
               <Link to='/'>Home</Link>
