0df5a773fa949e80e788269f637705e4a14a90fd
j-wenning <wenning.justin@gmail.com>
2020-11-30 12:20:45 -0800

    Updated search component to just wrap the product list

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,21 +2,9 @@ import React from 'react';
 import ProductList from './ProductList';
 
 export default class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { search: null };
-  }
-
-  componentDidMount() {
-    this.setState({
-      search: new URLSearchParams(document.location.search).get('s')
-    });
-  }
-
   render() {
     return (
       <main>
-        <h1>Search results for: <span>{ this.state.search || '...'}</span></h1>
         <ProductList />
       </main>
     );
