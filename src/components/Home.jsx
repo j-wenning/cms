5ea9cd22a087d556ee9b2dea524fd3b08b49fec3
j-wenning <wenning.justin@gmail.com>
2020-11-29 20:22:14 -0800

    Updated home component to remove bottom margin from jumbotron

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <main>
-        <div className='jumbotron mb-0 mb-lg-5'>
+        <div className='jumbotron mb-0'>
           <h1 className='display-4'>Today's Deals</h1>
           <hr className='my-4' />
           <Featured />
