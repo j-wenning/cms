7f8b3799ee928e292d6b83dbf5409a45ff9a3bb3
j-wenning <wenning.justin@gmail.com>
2020-11-29 19:07:02 -0800

    Updated uri script to have more predictable behaviour and handle empty queries

--- src/components/URI.js
+++ src/components/URI.js
@@ -2,23 +2,14 @@ export const parseQuery = query => {
   const result = {};
   (/^\?/.test(query) ? query.substr(1) : query).split('&').forEach(pair => {
     const [key, val] = pair.split('=');
-    result[key] = val;
+    if (key) result[key] = val;
   });
   return result;
 };
 
-export const buildQuery = (obj, query) => {
-  const resObj = {};
-  let result = '';
-  switch (typeof query) {
-    case typeof Object():
-    case typeof String():
-      break;
-    default:
-      return console.error('Invalid query type');
-  }
-  Object.assign(resObj, typeof query === typeof Object() ? query : parseQuery(query));
-  Object.assign(resObj, obj);
+export const buildQuery = (obj, query = '') => {
+  const resObj = Object.assign(parseQuery(query), obj);
+  let result = '?';
   for (const key in resObj) result += `${key}=${resObj[key]}&`;
-  return '?' + result.substr(0, result.length - 1);
+  return result.substr(0, result.length - 1);
 };
