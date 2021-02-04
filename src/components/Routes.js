5ba103caccd7b359e2e42d16669ba01847bd630a
j-wenning <wenning.justin@gmail.com>
2021-02-04 11:40:43 -0800

    Updated routes script to include routes for orders and renamed order component Updated script to remove unnecessary route props from all listed objects

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,44 +3,42 @@ import Search from './Search';
 import Product from './Product';
 import Cart from './Cart';
 import Checkout from './Checkout';
-import Receipt from './Receipt';
+import Order from './Order';
+import Orders from './Orders';
 import _404 from './404';
 
 const routes = [
   {
     path: '/',
     component: Home,
-    routes: []
   },
   {
     path: '/search',
     component: Search,
-    routes: []
   },
   {
     path: '/product',
     component: Product,
-    routes: []
   },
   {
     path: '/cart',
     component: Cart,
-    routes: []
   },
   {
     path: '/checkout',
     component: Checkout,
-    routes: []
   },
   {
-    path: '/receipt',
-    component: Receipt,
-    routes: []
+    path: '/orders/:oid',
+    component: Order,
+  },
+  {
+    path: '/orders',
+    component: Orders,
   },
   {
     path: '*',
     component: _404,
-    routes: []
   },
 ];
 
