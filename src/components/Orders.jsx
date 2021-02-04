0482a0800fc201d4ce7bee0784e1255e996494a1
j-wenning <wenning.justin@gmail.com>
2021-02-04 15:50:49 -0800

    Updated orders component to include redirect on empty order list

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { buildQuery } from './URI';
 
 class Orders extends React.Component {
   constructor(props) {
@@ -26,41 +27,53 @@ class Orders extends React.Component {
 
   render() {
     const { orders } = this.state;
-    const { prevLocation = 'back' } = this.props.location.state || {};
     return (
       <main>
         <div className='container mt-5'>
           <div className='row'>
             <div className='col'>
               <h1 className='mb-4'>Your Orders</h1>
-              <table className='table table-light table-bordered table-hover'>
-                <thead className='thead-light'>
-                  <tr>
-                    <th scope='col'>Submitted</th>
-                    <th scope='col'>Total cost</th>
-                    <th scope='col'></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    orders.map(order => {
-                      const { id, submitted, total } = order;
-                      const date = new Date(submitted);
-                      return (
-                        <tr key={id}>
-                          <td className='w-25'>{date.toLocaleString()}</td>
-                          <td>${total.toFixed(2)}</td>
-                          <td className='w-px-1'>
-                            <Link
-                              to={'/orders/' + id}
-                              className='btn btn-primary'>View</Link>
-                          </td>
-                        </tr>
-                      );
-                    })
-                  }
-                </tbody>
-              </table>
+              {
+                orders.length === 0
+                ? (
+                  <div>
+                    <p className='mb-4'>No orders are currently associated with your account.</p>
+                    <Link
+                      to={'/search' + buildQuery({ deals: 'true' })}
+                      className='btn btn-primary'>Find deals</Link>
+                  </div>
+                )
+                : (
+                  <table className='table table-light table-bordered table-hover'>
+                    <thead className='thead-light'>
+                      <tr>
+                        <th scope='col'>Submitted</th>
+                        <th scope='col'>Total cost</th>
+                        <th scope='col'></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {
+                        orders.map(order => {
+                          const { id, submitted, total } = order;
+                          const date = new Date(submitted);
+                          return (
+                            <tr key={id}>
+                              <td className='w-25'>{date.toLocaleString()}</td>
+                              <td>${total.toFixed(2)}</td>
+                              <td className='w-px-1'>
+                                <Link
+                                  to={'/orders/' + id}
+                                  className='btn btn-primary'>View</Link>
+                              </td>
+                            </tr>
+                          );
+                        })
+                      }
+                    </tbody>
+                  </table>
+                )
+              }
             </div>
           </div>
         </div>
